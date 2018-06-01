2f22c32d43f8c923a594c8b2c58ecbf695e9a55b
unknown <ehurst01@gmail.com>
2018-06-01 10:56:21 -0700

    added event handlers

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,22 @@
 window.onload = function(){ 
-    let scrumCard = document.getElementById("scrum-card");
-    let devCard = this.document.getElementById('dev-card');
-    let gameCard = this.document.getElementById('game-card');
+    const scrumCard = document.getElementById("scrum-card");
+    const devCard = this.document.getElementById('dev-card');
+    const gameCard = this.document.getElementById('game-card');
 
-    if(scrumCard) scrumCard.onclick = () => {window.open('https://scrumalliance.org/community/profile/ehurst3 ','_blank')};
-    if(devCard) devCard.onclick = () => {window.location='/portfolio'};
-    if(gameCard) gameCard.onclick = () => {window.open('https://boardgamegeek.com/user/Xer0Cool','_blank')};
 
+    scrumCard.addEventListener("click",(event)=>{
+        window.open('https://scrumalliance.org/community/profile/ehurst3 ','_blank')
+    });
+
+    devCard.addEventListener("click",(event)=>{
+        window.location='/portfolio'
+    });
+
+    gameCard.addEventListener("click",(event)=>{
+        window.open('https://boardgamegeek.com/user/Xer0Cool ','_blank')
+    });
+
+    
 
 
 };
